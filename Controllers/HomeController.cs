5dce19690914ebc02d80f7a2f3640a87eab21665
Mohamed Rashad <engineer.rachad@gmail.com>
2024-02-28 10:49:28 +0200

    Upload Files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace demo.Controllers
+{
+
+    //HomeController inhert Controller
+    public class HomeController : Controller
+    {
+        //constractour
+        public HomeController()
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //localhost:7277/home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        
+    }
+
+
+
+
+}
